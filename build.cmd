9e10cb3e3832c0c9a4b372fe57123d5d8a1d345b
Chris Culhane <chris@kneesystems.com>
2020-07-02 23:32:25 +1000

    Adding build spec and command

---
+++ build.cmd
@@ -0,0 +1,5 @@
+pyinstaller build_dir.spec --noconfirm
+REM pyinstaller build_onefile.spec --noconfirm
+mkdir .\dist\LectureSplit\input
+mkdir .\dist\LectureSplit\output
+robocopy .\tests\testdata .\dist\LectureSplit\input lecture_trimmed.mp4
--- lecture_split/main.py
+++ lecture_split/main.py
@@ -1,11 +1,12 @@
 import os
 import subprocess
+import sys
 import time
 from pathlib import Path
 from typing import Optional, List, Tuple
 
 import pprintpp as pprintpp
-from gooey import Gooey, GooeyParser
+from gooey import GooeyParser, Gooey
 from scenedetect import video_splitter, FrameTimecode
 from scenedetect.detectors import ContentDetector
 from scenedetect.scene_manager import SceneManager
@@ -28,8 +29,8 @@ def detect_scenes(video_pth: Path, scene_detection_threshold: int, min_scene_len
     :return:
     """
     time_start = time.time()
-    print(f"Starting detect_scene with args: \n"
-          f"{video_pth=}\n{scene_detection_threshold=}\n{min_scene_length=}\n{frame_skip=}")
+    print(f"\nStarting detect_scene with args: \n"
+          f"{video_pth=}\n{scene_detection_threshold=}\n{min_scene_length=}\n{frame_skip=}", flush=True)
     video_manager = VideoManager([str(video_pth)])
     scene_manager = SceneManager()
     # Add ContentDetector algorithm (constructor takes detector options like threshold).
@@ -41,16 +42,16 @@ def detect_scenes(video_pth: Path, scene_detection_threshold: int, min_scene_len
         video_manager.set_downscale_factor()  # no args means default
         video_manager.start()
 
-        scene_manager.detect_scenes(frame_source=video_manager, frame_skip=frame_skip)
+        scene_manager.detect_scenes(frame_source=video_manager, frame_skip=frame_skip, show_progress=False)
 
         # Obtain list of detected scenes.
         scene_list = scene_manager.get_scene_list(base_timecode)
         time_taken = time.time() - time_start
-        print(f"Processing complete in {round(time_taken, 1)} seconds")
-        print('List of scenes obtained:')
+        print(f"Detection of scenes complete in {round(time_taken, 1)} seconds", flush=True)
+        print('List of scenes obtained:', flush=True)
         for i, scene in enumerate(scene_list):
             print(f'    Scene {i + 1}: Start {scene[0].get_timecode()} / Frame {scene[0].get_frames()}, '
-                  f'End {scene[1].get_timecode()} / Frame {scene[1].get_frames()}')
+                  f'End {scene[1].get_timecode()} / Frame {scene[1].get_frames()}', flush=True)
         return scene_list
     finally:
         video_manager.release()
@@ -88,7 +89,7 @@ def extract_split_audio(video_pth: Path, output_dir: Path, scene_list: List[Tupl
         ret_val = subprocess.call(call_list)
 
         if not suppress_output and i == 0 and len(scene_list) > 1:
-            print('Output from ffmpeg for Scene 1 shown above, splitting remaining scenes...')
+            print('Output from ffmpeg for Scene 1 shown above, splitting remaining scenes...', flush=True)
         if ret_val != 0:
             break
 
@@ -100,36 +101,28 @@ def main():
                         default=str(PROJECT_DIR / "input"), widget="DirChooser")
     parser.add_argument('output_dir', help="Output directory where a new directory per video file will be created",
                         default=str(PROJECT_DIR / "output"), widget="DirChooser")
-    parser.add_argument('scene_detection_threshold',
+    parser.add_argument('--scene_detection_threshold',
                         help="Threshold for slide change detection. \n"
                              "1 is very sensitive, 30 is very un-sensitive, default 5",
-                        default=5, type=int)
-    parser.add_argument('scene_detection_threshold',
-                        help="Threshold for slide change detection. \n"
-                             "1 is very sensitive, 30 is very un-sensitive, default 5",
-                        default=5, type=int)
-    parser.add_argument('min_scene_length',
+                        default=5, type=int, required=False)
+    parser.add_argument('--min_scene_length',
                         help="minimum scene length (in frames). Default 100",
-                        default=100, type=int)
-    parser.add_argument('frame_skip',
+                        default=100, type=int, required=False)
+    parser.add_argument('--frame_skip',
                         help="Number of frames to skip, peeding up the detection time at the expense of accuracy. Default 5",
-                        default=5, type=int)
-    # INPUT_FILE = PROJECT_DIR / "tests" / "testdata" / "lecture_trimmed.mp4"
-    # OUTPUT_DIR = PROJECT_DIR / "output" / INPUT_FILE.stem
-    # SCENE_DETECTION_THRESHOLD = 5
-    # MIN_SCENE_LENGTH = 100  # In frames, default 100
-    # FRAME_SKIP = 5  # default 5
+                        default=5, type=int, required=False)
     args = parser.parse_args()
-    print("Starting with args: ")
-    pprintpp.pprint(args)
+    print("LectureSplit ", flush=True)
+    # pprintpp.pprint(args)
     video_pths = list(Path(args.input_dir).glob("*"))
-    for video_pth in video_pths:
+    print(f"Found {len(video_pths)} videos to process!")
+    for video_pth in tqdm(video_pths, file=sys.stdout, desc="Processing videos", position=0, miniters=1):
         detected_scenes = detect_scenes(video_pth=video_pth, scene_detection_threshold=args.scene_detection_threshold,
                                         min_scene_length=args.min_scene_length, frame_skip=args.frame_skip)
         extract_split_audio(video_pth=video_pth, output_dir=Path(args.output_dir), scene_list=detected_scenes,
                             suppress_output=True)
 
-    print(f"All processing complete, please check output files in {Path(args.output_dir).absolute()}")
+    print(f"All processing complete, please check output files in {Path(args.output_dir).absolute()}", flush=True)
 
 
 if __name__ == "__main__":
