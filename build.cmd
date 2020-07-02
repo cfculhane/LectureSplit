0b39bf6334d948e4a39ef5dd51daf1dd763ad6ed
Chris Culhane <chris@kneesystems.com>
2020-07-02 23:38:37 +1000

    Updating build command to zip resultant build

--- build.cmd
+++ build.cmd
@@ -3,3 +3,4 @@ REM pyinstaller build_onefile.spec --noconfirm
 mkdir .\dist\LectureSplit\input
 mkdir .\dist\LectureSplit\output
 robocopy .\tests\testdata .\dist\LectureSplit\input lecture_trimmed.mp4
+powershell "Compress-Archive .\dist\LectureSplit\ .\dist\LectureSplit.zip"
